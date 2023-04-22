0d0684b3d449dbeeafe800eca878295ad33d5ede
magicalcoder11 <gowrijayan11@gmail.com>
2023-04-22 16:53:26 +0530

    starting changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { tenureData } from "./utils/constants";
 
-function App() {
+const App = () => {
+  const [cost, setCost] = useState(0);
+  const [interest, setInterest] = useState(10);
+  const [fee, setFee] = useState(1);
+  const [downPayment, setDownPayment] = useState(0);
+  const [tenure, setTenure] = useState(12);
+  const [emi, setEmi] = useState(0);
+
+  const updateDownPayment = (e) => {
+    if (!cost) return;
+    const dp = Number(e.target.value);
+    setDownPayment(dp.toFixed(0));
+  };
+
+  const updateEMI = (e) => {
+    if (!cost) return;
+    const emi = Number(e.target.value);
+    setEmi(emi.toFixed(0));
+  };
+  const calculateEMI = () => {};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="title" style={{ fontSize: 30, marginTop: 20 }}>
+        Ecalc
+      </span>
+      <span className="title">Total cost of Asset</span>
+      <input
+        type="number"
+        value={cost}
+        onChange={(e) => setCost(e.target.value)}
+        placeholder="Total cost of Asset"
+      />
+      <span className="title">Interest Rate(in %)</span>
+      <input
+        type="number"
+        value={interest}
+        onChange={(e) => setInterest(e.target.value)}
+        placeholder="Interest Rate(in %)"
+      />
+      <span className="title">Processing Fee(in %)</span>
+      <input
+        type="number"
+        value={fee}
+        onChange={(e) => setFee(e.target.value)}
+        placeholder="Processing Fee(in %)"
+      />
+
+      <span className="title">Down Payment</span>
+      <div>
+        <input
+          type="range"
+          min={0}
+          max={cost}
+          className="slider"
+          value={downPayment}
+          onChange={updateDownPayment}
+        />
+        <div className="labels">
+          <label>0%</label>
+          <b>{downPayment}</b>
+          <label>100%</label>
+        </div>
+      </div>
+
+      <span className="title">Loan Per Month</span>
+      <div>
+        <input
+          type="range"
+          min={calculateEMI(cost)}
+          max={calculateEMI(0)}
+          className="slider"
+          value={emi}
+          onChange={updateEMI}
+        />
+        <div className="labels">
+          <label>{calculateEMI(cost)}</label>
+          <b>{emi}</b>
+          <label>{calculateEMI(0)}</label>
+        </div>
+      </div>
+
+      <span className="title">Tenure</span>
+      <div className="tenureContainer">
+        {tenureData.map((t) => {
+          return (
+            <button
+              className={`tenure ${t === tenure ? "selected" : ""}`}
+              onClick={() => setTenure(t)}
+            >
+              {t}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
